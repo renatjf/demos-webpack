6a4bbaf6df595af08db8128c3472ca8b3e6f86ea
Renato Freitas <renatjf@gmail.,com>
2017-02-13 19:09:39 -0200

    demos

---
+++ teste-oilib/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { PageOiDigital } from 'oilib'
+require('./app.css')
+
+
+class App extends Component {
+
+  render() {
+	return (
+	    <PageOiDigital>
+	    	teste
+	    </PageOiDigital>
+	)
+  }
+}
+
+export default App
---
+++ teste-oilib/webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: './main.jsx',
+    output: {
+        filename: 'bundle-oilib.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader')
+            }, {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("/app.css", {
+            allChunks: true
+        })
+    ],
+    postcss: [
+        autoprefixer({
+            browsers: ['last 2 version']
+        })
+    ]
+};
